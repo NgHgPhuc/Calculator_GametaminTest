5b513634a413ee052afcefcc1c83cdf3fa6205b1
phucdepzai156 <phucdepzai156@gmail.com>
2023-04-11 22:45:03 +0700

    99% without nothing

---
+++ Assets/Code/SpecialButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SpecialButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject PopupPanel;
+    public Image BackgroundApp;
+    public Sprite RickAstley;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if(transform.GetChild(0).GetComponent<TextMeshProUGUI>().text == "")
+            PopupPanel.SetActive(true);
+        else
+        {
+            BackgroundApp.sprite = RickAstley;
+        }
+    }
+    public void PopupBackgroundClick()
+    {
+        PopupPanel.SetActive(false);
+    }
+
+}
